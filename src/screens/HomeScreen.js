1e5ee65eefd0fbf9ca6c36424f0aa24d9d6f8f68
weniver <weniver@gmail.com>
2020-09-07 15:39:53 -0500

    Add readme

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const HomeScreen = () => {
+  return (
+    <div className="screen">
+      <h1 className="title">Home</h1>
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/screens/ODAsScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import '../App.css';
+
+const ODAsScreen = () => {
+  return (<div className="screen"><h1>ODAs</h1></div>);
+};
+
+export default ODAsScreen;

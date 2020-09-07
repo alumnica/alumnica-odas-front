f09378e5fbebe3a74e704733793854f65325e746
weniver <weniver@gmail.com>
2020-09-07 15:38:19 -0500

    First implementation of navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import ODAsScreen from "./screens/ODAsScreen.js"
+import HomeScreen from "./screens/HomeScreen.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <div className="App">
+          <nav className="nav-header">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/odas">Odas</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/odas">
+              <ODAsScreen />
+            </Route>
+            <Route path="/">
+              <HomeScreen/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>)
 }
 
 export default App;
